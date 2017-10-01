d801293b4d24f0a94c110aea558cac3ec738dc78
Dan Schrider <dan.schrider@rutgers.edu>
2017-10-01 19:32:35 -0400

    added script for vcf to fasta; added training data for 1000 Genomes scan

--- phasedVcfGenosToFasta.py
+++ phasedVcfGenosToFasta.py
@@ -1,7 +1,27 @@
 import sys, os, gzip
 
-popVcfFileName, refFileName,  targetChr, rangeStart, rangeEnd, popFaOutFileName = sys.argv[1:]
-rangeStart, rangeEnd = int(rangeStart), int(rangeEnd)
+try:
+    popVcfFileName, refFaFileName,  targetChr, rangeStart, rangeEnd, popFaOutFileName = sys.argv[1:]
+    rangeStart, rangeEnd = int(rangeStart), int(rangeEnd)
+except Exception:
+    usageStr="""phasedVcfGenosToFasta.py -- convert a population sample's phased genotypes in .vcf format to .fasta format
+usage:
+
+python popVcfFileName, refFaFileName,  targetChr, rangeStart, rangeEnd, popFaOutFileName
+
+arguments:
+
+popVcfFileName: the path to a VCF file containing phased genotypes for our sample (can be gzipped).
+refFaFileName: the path to a fasta file containing the reference sequence for our target chromosome only.
+targetChr: the name of the chromosome that we wish to examine (used for labeling fasta entries only).
+rangeStart and rangeEnd: the bounds of the region on targetChr that we wish to examine.
+popFaOutFileName: path to our output fasta, which includes monomorphic sites and has two entries per diploid.
+
+Note: To mask sites you must edit reference fasta file to have 'N' at sites you wish to ignore.
+Another note: This script assumes that all genotypes are imputed (ewww) as in the 1000 Genomes data.
+Final note: To use a subset of individuals in your VCF currently you must make a new file omitting the rest.
+"""
+    sys.exit(usageStr)
 
 sys.stderr.write("reading genotypes\n")
 
@@ -48,7 +68,7 @@ for sampleId in sampleIds:
 assert numChromos == len(hapIds)
 
 refChr = ""
-refFile = open(refFileName)
+refFile = open(refFaFileName)
 sys.stderr.write("reading reference chromosome\n")
 for line in refFile.xreadlines():
     if not line.startswith(">"):
@@ -61,8 +81,6 @@ for i in range(rangeStart-1, rangeEnd):
 
 sys.stderr.write("writing population sample sequence in fasta format\n")
 popFaOutFile = open(popFaOutFileName, "w")
-targetChr = targetChr.replace("NonPAR","")
-targetChr = targetChr.replace("PAR","")
 for i in range(numChromos):
     popFaOutFile.write(">%s_%s:%s-%s\n" %(hapIds[i], targetChr, rangeStart, rangeEnd))
     doneSites = 0
